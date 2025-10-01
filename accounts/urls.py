01cb23565a8ee1fd89a4d248990573d777c80cf9
JBrij <justinbrij@yahoo.com>
2025-10-01 18:06:12 -0400

    Registration complete page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.signup_view, name="signup"),
+    path('signup/', views.signup_view, name='signup'),
+    path('registration-complete/', views.registration_complete, name='registration_complete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,8 +8,13 @@ def signup_view(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("login")  # after signup, redirect to login
+            form.save() # create the user
+            return redirect('registration_complete')  # redirect to confirmation page
     else:
         form = CustomUserCreationForm()
     return render(request, "accounts/signup.html", {"form": form})
+
+# accounts/views.py
+def registration_complete(request):
+    return render(request, 'accounts/registration_complete.html')
+

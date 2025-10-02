101c69436f5603b29ed4911f23c57226917550c3
JBrij <justinbrij@yahoo.com>
2025-10-02 14:22:49 -0400

    registration updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("", views.home, name="home"),  # home view
+    path("", views.home_view, name="home"),  # home view
     path("signup/", views.signup_view, name="signup"),
     path("registration-complete/", views.registration_complete, name="registration_complete"),
+    path('login/', views.dummy_login, name='login'),  # temporary
+    path('logout/', views.dummy_logout, name='logout'),  #temporary
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 
+def dummy_login(request):
+    # This is just a placeholder for testing
+    return render(request, 'accounts/dummy_login.html')
+
+def dummy_logout(request):
+    return render(request, 'accounts/dummy_logout.html')
+
 def signup_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()  # saves the user
-            return redirect('registration_complete')
+            return redirect('accounts:registration_complete')
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
@@ -14,5 +21,5 @@ def signup_view(request):
 def registration_complete(request):
     return render(request, 'accounts/registration_complete.html')
 
-def home(request):
+def home_view(request):
     return render(request, 'accounts/home.html')
